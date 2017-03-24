916d83345c3a4f94232ffb8d057ace958bd4272f
Ana Caroline França <acff@cin.ufpe.br>
2017-03-23 22:49:28 -0300

    Update/Complete problema2.js

--- lista1/problema2.js
+++ lista1/problema2.js
@@ -1,13 +1,14 @@
 var mySVG;
 var xScale;
 var yScale;
+var currentLen = 0;
 var cScale = d3.scaleLinear().domain([0,100]).range(["gray", "blue"]);
 
 window.onload = function() {
   var margin = {top: 50, right: 50, bottom:50, left: 50};
 
-  var width = 500- margin.left - margin.right;
-  var height = 500 - margin.top - margin.bottom;
+  var width = 800- margin.left - margin.right;
+  var height = 600 - margin.top - margin.bottom;
 
   xScale = d3.scaleLinear().domain([0,100]).range([0,width]);
   yScale = d3.scaleLinear().domain([0,100]).range([height, 0]);
@@ -35,12 +36,13 @@ function getRandom(min,max)
   return Math.floor(Math.random()*(max - min + 1)) + min;
 }
 
-function generateData(){
-  mySVG.selectAll("circle").remove()
+function generateData()
+{
   var dataset = [];
   var datasetLength = getRandom(1, 50);
 
-  for(i = 0; i < datasetLength; i++){
+  for(i = 0; i < datasetLength; i++)
+  {
     var x = getRandom(1, 100);
     var y = getRandom(1, 100);
     var z = getRandom(1, 100)/10;//radius
@@ -49,17 +51,34 @@ function generateData(){
     dataset = dataset.concat([[x ,y, z, w]]);
   }
 
-  d3
-    .select("h3")
-    .html('Data length > ' + datasetLength);
+  d3 .select("h3")
+    .html('Data length > ' + datasetLength); 
 
+  if(datasetLength > currentLen)
+  {
+    mySVG
+      .selectAll("circle")
+      .data(dataset)
+      .enter()
+      .append("circle")
+      .attr("r",function(d){return xScale(d[2]/5)})
+      .attr("cx",function(d){return xScale(d[0]);})   
+      .attr("cy",function(d){return yScale(d[1]);})
+      .attr("fill", function(d){return cScale(d[3])});
+      currentLen = datasetLength;
+  } else if(datasetLength < currentLen)
+  {
+     mySVG
+      .selectAll("circle")
+      .data(dataset)
+      .exit().remove();
+      currentLen = datasetLength;
+  }
   mySVG
-  .selectAll("circle")
-  .data(dataset)
-  .enter()
-  .append("circle")
-  .attr("r",function(d){return xScale(d[2]/5)})
-  .attr("cx",function(d){return xScale(d[0]);})   
-  .attr("cy",function(d){return yScale(d[1]);})
-  .attr("fill", function(d){return cScale(d[3])});  
+    .selectAll("circle")
+    .data(dataset)
+    .attr("r",function(d){return xScale(d[2]/5)})
+    .attr("cx",function(d){return xScale(d[0]);})   
+    .attr("cy",function(d){return yScale(d[1]);})
+    .attr("fill", function(d){return cScale(d[3])});     
 }
